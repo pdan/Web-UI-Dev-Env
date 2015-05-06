01d04f69f47ddd3ff291eda70e00cfd1347ff745
Pourya Daneshvar <pourya@daneshvar.me>
2015-05-06 16:32:00 +0430

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Web-UI-Dev-Environment
-Make a Web UI development environment in an Unix-like OS
+# Web UI dev environment
+
+This script helps web UI developers to making an development environment easy and fast. After install user has many tools like [Atom Editor](https://atom.io), [npm](https://www.npmjs.com/), [Grunt](https://gruntjs.com/), [Gulp](http://gulpjs.com/), [Bower](http://bower.io/)
+
+#### Note
+
+This script currently test on Ubuntu 14.04 only.
+
+
+
+## Getting Started
+
+Open your favorite Command Line and run these commands:
+
+``` bash
+$ git clone git@github.com:pdan/web-ui-dev-environment.git
+$ chmod u+x web-ui-dev-environment/install.sh
+$ ./web-ui-dev-environment/install.sh
+```
+
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y git build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
+
+sudo mkdir -p /usr/local/{share/man,bin,lib/node,include/node,lib/node_modules}
+sudo chown -R $USER /usr/local/{share/man,bin,lib/node,include/node,lib/node_modules}
+
+cd ~/Downloads
+
+mkdir node-install
+curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+cd node-install/*
+./configure
+make && make install
+
+source ~/.bashrc
+
+cd ~/Downloads
+wget https://www.npmjs.org/install.sh
+chmod +x install.sh
+. ./install.sh
+
+npm i -g grunt-cli gulp bower yo
+
+wget -c https://github.com/atom/atom/releases/download/v0.196.0/atom-amd64.deb
+sudo dpkg --install atom-amd64.deb
